276f697bfb7e2e63f805fba3362fbd12ddeac276
Dinowel <aimanmuqri18@gmail.com>
2023-02-12 01:48:21 +0800

    sunlight

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ class App{
 		this.scene = new THREE.Scene();
         this.scene.add( this.dolly );
         
-		const ambient = new THREE.HemisphereLight(0xeaec36, 0xfd7a05, 0.8);
+		const ambient = new THREE.HemisphereLight(0xe67305, 0xbc2f1a, 0.8);
 		this.scene.add(ambient);
 
       
